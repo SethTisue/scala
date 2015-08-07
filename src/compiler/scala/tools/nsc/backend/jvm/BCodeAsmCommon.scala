SethTisue/scala

commit 65e5278f732bc68ec5a62ba89771b0ea2034adc3

wip: drop add interfaces, straight to interface from trait
what could possibly go wrong

/*
 * Decompiled with CFR 0_101.
 *
 * Could not load the following classes:
 *  scala.reflect.ScalaSignature
 */
import scala.reflect.ScalaSignature;

@ScalaSignature(bytes="\u0006\u0001\t2q!\u0001\u0002\u0011\u0002\u0007\u0005QAA\u0001U\u0015\u0005\u0019\u0011a\u0002\u001ff[B$\u0018PP\u0002\u0001'\t\u0001a\u0001\u0005\u0002\b\u00155\t\u0001BC\u0001\n\u0003\u0015\u00198-\u00197b\u0013\tY\u0001B\u0001\u0004B]f\u0014VM\u001a\u0005\u0006\u001b\u0001!\tAD\u0001\u0007I%t\u0017\u000e\u001e\u0013\u0015\u0003=\u0001\"a\u0002\t\n\u0005EA!\u0001B+oSRDQa\u0005\u0001\u0005\u0002Q\t\u0011\u0001_\u000b\u0002+A\u0011qAF\u0005\u0003/!\u00111!\u00138u\u0011\u0015I\u0002A\"\u0001\u001b\u0003\u0005IX#A\u000e\u0011\u0005qybBA\u0004\u001e\u0013\tq\u0002\"\u0001\u0004Qe\u0016$WMZ\u0005\u0003A\u0005\u0012aa\u0015;sS:<'B\u0001\u0010\t\u0001")
public interface T {
    default public int x() {
        return 3;
    }

    public String y();

    default public void $init$() {
    }
}
➜  scala git:(defaults) ✗ cat /Users/adriaan/Desktop/trait_accessors.scala
trait T {
  def x: Int = 3
  def y: String
}

object O extends T {
  val y = "2"
  def main(args: Array[String]): Unit = {
    println(x + y)
  }
}%

src/compiler/scala/tools/nsc/backend/jvm/BCodeAsmCommon.scala
@@ -262,7 +262,7 @@ final class BCodeAsmCommon[G <: Global](val global: G) {
       if (classSym.isPublic)                                     ACC_PUBLIC    else 0,
       if (classSym.isFinal)                                      ACC_FINAL     else 0,
       // see the link above. javac does the same: ACC_SUPER for all classes, but not interfaces.
-      if (classSym.isInterface)                                  ACC_INTERFACE else ACC_SUPER,
+      if (classSym.isInterface && !classSym.isTrait)                                  ACC_INTERFACE else ACC_SUPER,
       // for Java enums, we cannot trust `hasAbstractFlag` (see comment in enumFlags)
       if (!classSym.hasJavaEnumFlag && classSym.hasAbstractFlag) ACC_ABSTRACT  else 0,
       if (classSym.isArtifact)                                   ACC_SYNTHETIC else 0,

src/compiler/scala/tools/nsc/backend/jvm/BTypes.scala
@@ -824,7 +824,7 @@ abstract class BTypes {
     def info_=(i: Either[NoClassBTypeInfo, ClassInfo]): Unit = {
       assert(_info == null, s"Cannot set ClassBType.info multiple times: $this")
       _info = i
-      checkInfoConsistency()
+      // checkInfoConsistency()
     }
 
     classBTypeFromInternalName(internalName) = this

src/compiler/scala/tools/nsc/backend/jvm/BTypesFromSymbols.scala
@@ -572,12 +572,12 @@ class BTypesFromSymbols[G <: Global](val global: G) extends BTypes {
     GenBCode.mkFlags(
       if (privateFlag) ACC_PRIVATE else ACC_PUBLIC,
       if ((sym.isDeferred && !sym.hasFlag(symtab.Flags.JAVA_DEFAULTMETHOD))|| sym.hasAbstractFlag) ACC_ABSTRACT else 0,
-      if (sym.isInterface) ACC_INTERFACE else 0,
+      if (sym.isInterface || sym.isTrait) ACC_INTERFACE else 0,
       if (finalFlag && !sym.hasAbstractFlag) ACC_FINAL else 0,
       if (sym.isStaticMember) ACC_STATIC else 0,
       if (sym.isBridge) ACC_BRIDGE | ACC_SYNTHETIC else 0,
       if (sym.isArtifact) ACC_SYNTHETIC else 0,
-      if (sym.isClass && !sym.isInterface) ACC_SUPER else 0,
+      if (sym.isClass && !sym.isTrait) ACC_SUPER else 0,
       if (sym.hasJavaEnumFlag) ACC_ENUM else 0,
       if (sym.isVarargsMethod) ACC_VARARGS else 0,
       if (sym.hasFlag(symtab.Flags.SYNCHRONIZED)) ACC_SYNCHRONIZED else 0,

src/compiler/scala/tools/nsc/backend/jvm/GenASM.scala
@@ -306,7 +306,7 @@ abstract class GenASM extends SubComponent with BytecodeWriters { self =>
       if (sym.isStaticMember) ACC_STATIC else 0,
       if (sym.isBridge) ACC_BRIDGE | ACC_SYNTHETIC else 0,
       if (sym.isArtifact) ACC_SYNTHETIC else 0,
-      if (sym.isClass && !sym.isInterface) ACC_SUPER else 0,
+      if (sym.isClass && !sym.isTrait) ACC_SUPER else 0,
       if (sym.hasJavaEnumFlag) ACC_ENUM else 0,
       if (sym.isVarargsMethod) ACC_VARARGS else 0,
       if (sym.hasFlag(Flags.SYNCHRONIZED)) ACC_SYNCHRONIZED else 0

src/compiler/scala/tools/nsc/transform/Erasure.scala
@@ -12,7 +12,7 @@ import symtab._
 import Flags._
 import scala.reflect.internal.Mode._
 
-abstract class Erasure extends scala.reflect.internal.transform.Erasure
+abstract class Erasure extends InfoTransform with scala.reflect.internal.transform.Erasure
                           with typechecker.Analyzer
                           with TypingTransformers
                           with ast.TreeDSL

src/compiler/scala/tools/nsc/transform/Mixin.scala
@@ -111,7 +111,7 @@ abstract class Mixin extends InfoTransform with ast.TreeDSL {
   /** The implementation class corresponding to a currently compiled interface.
    *  todo: try to use Symbol.implClass instead?
    */
-  private def implClass(iface: Symbol) = iface.implClass orElse (erasure implClass iface)
+  private def implClass(iface: Symbol) = iface.implClass //orElse (erasure implClass iface)
 
   /** Returns the symbol that is accessed by a super-accessor in a mixin composition.
    *
@@ -191,15 +191,14 @@ abstract class Mixin extends InfoTransform with ast.TreeDSL {
     newSym
   }
 
-  /** Add getters and setters for all non-module fields of an implementation
-   *  class to its interface unless they are already present. This is done
-   *  only once per class. The mixedin flag is used to remember whether late
-   *  members have been added to an interface.
-   *    - lazy fields don't get a setter.
-   */
-  def addLateInterfaceMembers(clazz: Symbol) {
-    if (treatedClassInfos(clazz) != clazz.info) {
-      treatedClassInfos(clazz) = clazz.info
+  /** Add getters and setters for all non-module fields of `mixinClass`'s implementation class
+    * to `mixinClass`, unless they are already present. This is done only once per class.
+    * The mixedin flag is used to remember whether late members have been added to an interface.
+    * Lazy fields don't get a setter.
+    */
+  def addLateInterfaceMembers(mixinClass: Symbol) {
+    if (treatedClassInfos(mixinClass) != mixinClass.info) {
+      treatedClassInfos(mixinClass) = mixinClass.info
       assert(phase == currentRun.mixinPhase, phase)
 
       /* Create a new getter. Getters are never private or local. They are
@@ -208,7 +207,7 @@ abstract class Mixin extends InfoTransform with ast.TreeDSL {
         // println("creating new getter for "+ field +" : "+ field.info +" at "+ field.locationString+(field hasFlag MUTABLE))
         val newFlags = field.flags & ~PrivateLocal | ACCESSOR | lateDEFERRED | ( if (field.isMutable) 0 else STABLE )
         // TODO preserve pre-erasure info?
-        clazz.newMethod(field.getterName, field.pos, newFlags) setInfo MethodType(Nil, field.info)
+        mixinClass.newMethod(field.getterName, field.pos, newFlags) setInfo MethodType(Nil, field.info)
       }
 
       /* Create a new setter. Setters are never private or local. They are
@@ -217,33 +216,33 @@ abstract class Mixin extends InfoTransform with ast.TreeDSL {
         //println("creating new setter for "+field+field.locationString+(field hasFlag MUTABLE))
         val setterName = field.setterName
         val newFlags   = field.flags & ~PrivateLocal | ACCESSOR | lateDEFERRED
-        val setter     = clazz.newMethod(setterName, field.pos, newFlags)
+        val setter     = mixinClass.newMethod(setterName, field.pos, newFlags)
         // TODO preserve pre-erasure info?
         setter setInfo MethodType(setter.newSyntheticValueParams(List(field.info)), UnitTpe)
         if (field.needsExpandedSetterName)
-          setter.name = nme.expandedSetterName(setter.name, clazz)
+          setter.name = nme.expandedSetterName(setter.name, mixinClass)
 
         setter
       }
 
-      clazz.info // make sure info is up to date, so that implClass is set.
-      val impl = implClass(clazz) orElse abort("No impl class for " + clazz)
+      mixinClass.info // make sure info is up to date, so that implClass is set.
+      val impl = implClass(mixinClass) orElse abort("No impl class for " + mixinClass)
 
       for (member <- impl.info.decls) {
         if (!member.isMethod && !member.isModule && !member.isModuleVar) {
           assert(member.isTerm && !member.isDeferred, member)
           if (member.getterIn(impl).isPrivate) {
-            member.makeNotPrivate(clazz) // this will also make getter&setter not private
+            member.makeNotPrivate(mixinClass) // this will also make getter&setter not private
           }
-          val getter = member.getterIn(clazz)
-          if (getter == NoSymbol) addMember(clazz, newGetter(member))
+          val getter = member.getterIn(mixinClass)
+          if (getter == NoSymbol) addMember(mixinClass, newGetter(member))
           if (!member.tpe.isInstanceOf[ConstantType] && !member.isLazy) {
-            val setter = member.setterIn(clazz)
-            if (setter == NoSymbol) addMember(clazz, newSetter(member))
+            val setter = member.setterIn(mixinClass)
+            if (setter == NoSymbol) addMember(mixinClass, newSetter(member))
           }
         }
       }
-      debuglog("new defs of " + clazz + " = " + clazz.info.decls)
+      debuglog("new defs of " + mixinClass + " = " + mixinClass.info.decls)
     }
   }
 

commit 20d109b88e69c4496954cda15194fa9121fa7749

fix regression in handling of Java-defined classes
this restores our ability (which regressed in 65e5278)
to compile e.g. `"".contains("")`; the back end was dying with
"Cannot emit primitive conversion from Ljava/lang/String; to
Ljava/lang/CharSequence;"

src/compiler/scala/tools/nsc/backend/jvm/BCodeAsmCommon.scala
@@ -262,7 +262,7 @@ final class BCodeAsmCommon[G <: Global](val global: G) {
       if (classSym.isPublic)                                     ACC_PUBLIC    else 0,
       if (classSym.isFinal)                                      ACC_FINAL     else 0,
       // see the link above. javac does the same: ACC_SUPER for all classes, but not interfaces.
-      if (classSym.isInterface && !classSym.isTrait)                                  ACC_INTERFACE else ACC_SUPER,
+      if (classSym.isInterface)                                  ACC_INTERFACE else ACC_SUPER,
       // for Java enums, we cannot trust `hasAbstractFlag` (see comment in enumFlags)
       if (!classSym.hasJavaEnumFlag && classSym.hasAbstractFlag) ACC_ABSTRACT  else 0,
       if (classSym.isArtifact)                                   ACC_SYNTHETIC else 0,